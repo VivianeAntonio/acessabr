621ffccdacca0014a1e0c7d01a85a00e3abdc1d7
VivianeAntonio <asilvaviviane@gmail.com>
2021-11-24 13:13:05 -0300

    update component curator

---
+++ src/components/Curator/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles.scss'
+import curatorPhoto from '../../images/viviane-antonio.jpg'
+
+function CuratorItem() {
+
+    return(
+        <div className='curator__container'>
+            <div className='curator__photo'>
+                <img src={curatorPhoto} alt='Foto da curadora Viviane Antonio da Silva'/>
+            </div>
+            <div className='curator__details'>
+                <div>
+                    <h3>Viviane Antonio da Silva</h3>
+                    <h6>Curadoria</h6>
+                </div>                
+                <p>Desenvolvedora fullstack formada em Análise e Desenvolvimento de Sistemas.</p>
+            </div>
+        </div>
+    )
+}
+
+export default CuratorItem
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/global.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Curator from './components/Curator';
+
 
 const CONFIG = {
   state: 'SP',
@@ -14,6 +16,8 @@ ReactDOM.render(
     <div className='main__container'>
       <Header city={CONFIG.city} state={CONFIG.state}/>
       <Home city={CONFIG.city}/>
+      {/* Espaço para inserir a lista de lugares */}
+      <Curator/>
 
     </div>
 
