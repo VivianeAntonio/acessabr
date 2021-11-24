6ab786983ac46b230ea83c3ce2011036340f350a
VivianeAntonio <asilvaviviane@gmail.com>
2021-11-24 10:59:32 -0300

    implementing react props

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,14 @@ import './style.scss';
 import logoAcessaBr from '../../images/logo-acessabr.svg'
 import logoEbac from '../../images/logo-ebac.svg'
 
-const Header = () => {
+const Header = props => {
     return(
         <header className="header__container">
 
             <div className="header__logo header__logo--state">
                 <img src={logoAcessaBr} alt="Logo AcessaBR"/>
                 <span className="header__city"> 
-                    / São Paulo - SP 
+                    / {props.city} - {props.state} 
                 </span>
 
             </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,14 +16,14 @@ const PLACES = [
     'Aquário'
 ]
 
-const Home = () => {
+const Home = propos => {
 
     const [selectedPill, setSelectedPill] = React.useState('')
 
     return (
         <main id="main-content" className="home__container">
             <div className="home__col">
-                <h1 className="home__title">São Paulo para todos</h1>
+                <h1 className="home__title">{propos.city} para todos</h1>
                 <p className="home__text">
                     Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa. 
                 </p>                
