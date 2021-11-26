2a67a265d35d933d75a461c5e51c52b0806b6602
VivianeAntonio <asilvaviviane@gmail.com>
2021-11-25 22:02:54 -0300

    update responsive

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,6 +24,11 @@ const Home = propos => {
         <main id="main-content" className="home__container">
             <div className="home__col">
                 <h1 className="home__title">{propos.city} para todos</h1>
+
+                <div className="home__image home__image--destack hide-desktop">
+                    <img src={imageDestack} alt='Imagem destaque cadeirante'/>
+                </div>
+
                 <p className="home__text">
                     Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa. 
                 </p>                
@@ -46,9 +51,8 @@ const Home = propos => {
                 </div> 
             </div>
             <div className="home__col">
-                <div className="home__image home__image--destack">
+                <div className="home__image home__image--destack hide-mobile">
                     <img src={imageDestack} alt='Imagem destaque cadeirante'/>
-
                 </div>
 
             </div>
